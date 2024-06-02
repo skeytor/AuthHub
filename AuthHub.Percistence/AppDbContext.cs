91907e0755634b288baa3e62787fec9e65c85658
Rober Guerrero <tj.rober.guerrero.m@upds.net.bo>
2024-06-02 16:26:52 -0400

    Add db connection string in Persistence layer

---
+++ AuthHub.Percistence/AppDbContext.cs
@@ -0,0 +1,6 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AuthHub.Persistence
+{
+    public sealed class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options) { }
+}
---
+++ AuthHub.Percistence/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuthHub.Persistence
+{
+    public static class DependencyInjection
+    {
+        private const string _connectionName = "Default";
+        public static IServiceCollection AddDataAccess(
+            this IServiceCollection services,
+            IConfiguration configuration) 
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString(_connectionName)));
+
+            return services;
+        }
+    }
+}
