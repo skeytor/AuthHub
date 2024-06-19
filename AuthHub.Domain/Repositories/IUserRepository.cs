59c46ca119bc45c1999a96986050a35ff32f5a95
Rober Guerrero <tj.rober.guerrero.m@upds.net.bo>
2024-06-19 15:46:44 -0400

    Add tests for the UserRepository

---
+++ AuthHub.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using AuthHub.Domain.Entities;
+
+namespace AuthHub.Domain.Repositories;
+public interface IUserRepository
+{
+    Task<User> CreateAsync(User user);
+    Task<User?> GetByIdAsync(Guid id);
+    Task DeleteAsync(Guid id);
+    Task<User> UpdateAsync(Guid id, User user);
+    Task<List<User>> GetAllAsync();
+}
--- AuthHub.Percistence/DependencyInjection.cs
+++ AuthHub.Percistence/DependencyInjection.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AuthHub.Domain.Repositories;
+using AuthHub.Persistence.Abstractions;
+using AuthHub.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,11 +12,11 @@ namespace AuthHub.Persistence
         private const string _connectionName = "Default";
         public static IServiceCollection AddDataAccess(
             this IServiceCollection services,
-            IConfiguration configuration) 
+            IConfiguration configuration)
         {
             services.AddDbContext<AppDbContext>(options =>
                 options.UseSqlServer(configuration.GetConnectionString(_connectionName)));
-
+            services.AddScoped<IAppDbContext>(options => options.GetRequiredService<AppDbContext>());
             return services;
         }
     }
