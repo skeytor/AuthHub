3dddd1c80f9311afc10ccd4667575c68ad168c40
Rober Guerrero <tj.rober.guerrero.m@upds.net.bo>
2024-06-29 21:53:30 -0400

    Change sctructure for tests

--- AuthHub.Percistence/Abstractions/IAppDbContext.cs
+++ AuthHub.Percistence/Abstractions/IAppDbContext.cs
@@ -1,13 +1,12 @@
 ﻿using AuthHub.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace AuthHub.Persistence.Abstractions
+namespace AuthHub.Persistence.Abstractions;
+
+public interface IAppDbContext
 {
-    public interface IAppDbContext
-    {
-        DbSet<User> Users { get; set; }
-        DbSet<Role> Roles { get; set; }
-        DbSet<Permission> Permissions { get; set; }
-        DbSet<RolePermission> RolePermissions { get; set; }
-    }
+    DbSet<User> Users { get; set; }
+    DbSet<Role> Roles { get; set; }
+    DbSet<Permission> Permissions { get; set; }
+    DbSet<RolePermission> RolePermissions { get; set; }
 }
